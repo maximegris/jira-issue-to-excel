a4718e3d065552049b9206b0efc07f9a7892999c
Maxime GRIS <m.gris@emotic.fr>
2017-12-08 11:33:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Extraction JIRA Issue en Excel (par projet et numéro de version)
+
+node index.js -u m.gris@emotic.fr:pass -p MAPETITEENERGIE -v 2.9.1,2.9.3
---
+++ index.js
@@ -0,0 +1,59 @@
+const request = require('request-promise');
+const argv = require('minimist')(process.argv.slice(2));
+const utf8 = require('utf8');
+const base64 = require('base-64');
+const dateFormat = require('dateformat');
+const json2xls = require('json2xls');
+const fs = require('fs');
+
+if (!argv.p || !argv.p || !argv.v) {
+    console.error('Usage:' + __filename + '-u username:password -p MONPROJET -v Version1,Version2,Version3');
+} else {
+
+    var bytes = utf8.encode(argv.u);
+    var auth = base64.encode(bytes);
+
+    const options = {
+        method: 'GET',
+        uri: 'https://emotic.atlassian.net/rest/api/2/search?jql=project=' + argv.p + ' AND fixVersion IN (' + argv.v + ') ORDER BY fixVersion,status,type,priority&fields=summary,description,issuetype,fixVersions,created,priority,updated,status',
+        json: true,
+        headers: {
+            'Authorization': 'Basic ' + auth,
+            'Content-Type': 'application/json'
+        }
+    }
+
+    request(options)
+        .then(function (response) {
+            
+            const data = [];
+            const issues = response.issues || [];
+            for(let i = 0 ; i < issues.length ;  i++) {
+                let issue = issues[i];
+                data.push({
+                    key: issue.key,
+                    summary : issue.fields.summary,
+                    description: issue.fields.description,
+                    type : issue.fields.issuetype.name,
+                    status : issue.fields.status.name,
+                    created : issue.fields.created,
+                    updated: issue.fields.updated,
+                    priority: issue.fields.priority.name,
+                    fixVersion: issue.fields.fixVersions && issue.fields.fixVersions.length > 0 ? issue.fields.fixVersions[0].name : ''
+                });
+            }
+
+            if(data.length > 0) {
+                const xls = json2xls(data);
+    
+                const now = Date();
+                const fileName = dateFormat(now, 'yyyy_mm_dd-HH_MM_ss_') + argv.p + '.xlsx';
+    
+                fs.writeFileSync(fileName, xls, 'binary');
+            }
+            
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+}
